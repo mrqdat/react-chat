f6957e82381b9dc923cf4b763abeaaa6c3a2fe6f
Đạt Nguyễn <Q.Dat@outlook.com>
2020-09-15 11:44:23 +0700

    a

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
  const express  = require('express');
  const socketio = require('socket.io');
  const http = require('http');
-
+ const cors = require('cors');
  const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
  const PORT =  process.env.PORT || 5000;
@@ -12,18 +12,20 @@
  const server = http.createServer(app);
  const io = socketio(server);
 
+ app.use(cors());
+ app.use(router);
  io.on('connect', (socket) => {
     console.log('new connection');
 
-    socket.on('Join', ({ name, room }, callback) =>{
+    socket.on('Join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room});  
         
         if(error) return callback(error);
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
         socket.join(user, room);
-
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
+        
         io.to(user.room).emit('roomData', { room: user.room, user: getUsersInRoom(user.room)});
 
         callback();
@@ -33,7 +35,7 @@
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message});
-        io.to(user.room).emit('roomData', { room: user.room, user: getUsersInRoom(user.room)}); 
+        
         callback();
     });
 
@@ -43,10 +45,10 @@
 
         if(user){
             io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left the chat`});
+            io.to(user.room).emit('roomData', { room: user.room, user: getUsersInRoom(user.room)});
         }
     });
  });
 
- app.use(router);
 
  server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
